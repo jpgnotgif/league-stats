1a6c3c16c8b390c15a2bde9da69eb6e2fb201556
Joe Gutierrez <josephpaulgutierrez@gmail.com>
2016-03-12 12:55:57 -0800

    display of kills, death, assists and kd ratio from the game-v1.3 endpoint

--- controllers/summoner.js
+++ controllers/summoner.js
@@ -3,25 +3,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET stats listing listing. */
-router.post('/', function(req, res, next) {
-
+router.post('/stats', function(req, res, next) {
   var id;
   var stats;
-
   var summonerName = req.body.name;
   var summoner = new summonerModel(summonerName);
   summoner.requestId( function(id) {
-    summoner.id = id;
+    // TODO: error handling
 
+    summoner.id = id;
     summoner.requestStats( function(stats) {
       summoner.stats = stats;
-
-      res.json({name: summoner.name, id: id, stats: stats});
-
+      res.render('summoner/stats', {name: summoner.name, stats: stats})
     });
-
   });
-
 });
 
 module.exports = router;
--- models/summoner.js
+++ models/summoner.js
@@ -1,5 +1,7 @@
 var request = require('request');
+var math = require('mathjs');
 
+// TODO: Productionize!
 var queryParams = {
   api_key: "efecd9e1-f295-4690-9dde-0709791bccad"
 };
@@ -8,7 +10,10 @@ var Summoner = function(name) {
   this.name = name.toLowerCase();
   this.requestId = function(callback) {
     var name = this.name;
+
+    // TODO: Productionize!
     var baseUrl = `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${name}`;
+
     request( {url: baseUrl, qs: queryParams} , function(error, response, body) {
       var id;
       if (!error && response.statusCode == 200) {
@@ -23,24 +28,22 @@ var Summoner = function(name) {
 
   this.requestStats = function(callback) {
     var id = this.id;
+
+    // TODO: Productionize!
     var baseUrl = `https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/${id}/recent`;
 
     var gameStats = [];
-
     request( {url: baseUrl, qs: queryParams}, function(error, response, body) {
-
       jsonBody = JSON.parse(body)
       jsonBody['games'].forEach( function(gameData, index, array) {
 
-        var stats = gameData['stats'];
-        console.log(stats);
-
-        var kills = stats['championsKilled'];
-        var deaths = stats['numDeaths'];
-        var assists = stats['assists'];
-
-        // TODO: calculate kd ratio as (k+a)/d
-        gameStats.push({kills: kills, deaths: deaths, assists: assists});
+        // TODO: error handling! what happens if these values are undefined?
+        var stats     = gameData['stats'];
+        var kills     = stats['championsKilled'];
+        var deaths    = stats['numDeaths'];
+        var assists   = stats['assists'];
+        var kda_ratio = math.round((kills + assists)/deaths, 3);
+        gameStats.push({kills: kills, deaths: deaths, assists: assists, kda_ratio: kda_ratio});
 
       })
       callback(gameStats);
